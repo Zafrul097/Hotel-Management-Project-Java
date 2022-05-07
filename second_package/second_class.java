015d998a3c069faef754449895cf85b9cf62671d
Zafrul097 <zafrulislamridoy@gmail.com>
2022-05-07 22:08:38 +0600

    created second class in B1 branch

---
+++ second_package/second_class.java
@@ -0,0 +1,54 @@
+package second_package;
+
+public class second_class {
+    int a,b,c;
+    float d,e;
+
+    public second_class(int a, int b, int c, float d, float e) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getC() {
+        return c;
+    }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public float getD() {
+        return d;
+    }
+
+    public void setD(float d) {
+        this.d = d;
+    }
+
+    public float getE() {
+        return e;
+    }
+
+    public void setE(float e) {
+        this.e = e;
+    }
+}
